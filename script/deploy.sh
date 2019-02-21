6926df62ceae3b3ce48ec901d373c50ee795e63f
root <root@ip-172-31-42-168.us-east-2.compute.internal>
2019-02-21 10:50:10 +0000

    chnage in sudo comand

--- script/deploy.sh
+++ script/deploy.sh
@@ -4,11 +4,11 @@ ssh ubuntu@18.217.98.110 <<EOF
     cd ~/financial-planer
     sudo git pull origin master
     sudo curl -o-   https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh    | bash
-    sudo . ~/.nvm/nvm.sh
-    sudo nvm install v10.11.0
-    sudo npm install
-    sudo npm install -g nodemon pm2
-    sudo pm2 restart ecosystem.config.js
+    . ~/.nvm/nvm.sh
+    nvm install v10.11.0
+    npm install
+    npm install -g nodemon pm2
+    pm2 restart ecosystem.config.js
     exit
 EOF
 
