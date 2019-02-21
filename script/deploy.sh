5393105a38b47a2b926564b67b8b066dd1827a1d
root <root@ip-172-31-42-168.us-east-2.compute.internal>
2019-02-21 10:01:52 +0000

    replace new deploy file

---
+++ script/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+ssh ubuntu@18.217.98.110 <<EOF
+    cd ~/financial-planer
+    git pull origin master
+    curl -o-   https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh    | bash
+    . ~/.nvm/nvm.sh
+    nvm install v10.11.0
+    npm install
+    npm install -g nodemon pm2
+    pm2 restart ecosystem.config.js
+    exit
+EOF
+
