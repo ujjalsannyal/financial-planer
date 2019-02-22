bbb46db5a0c284172ca7c3b6a504a39c11e5d522
Ubuntu <ubuntu@ip-172-31-42-168.us-east-2.compute.internal>
2019-02-22 09:07:39 +0000

    change in deploy code

--- script/deploy.sh
+++ script/deploy.sh
@@ -6,6 +6,7 @@ ssh ubuntu@18.223.111.203 <<EOF
     curl -o-   https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh    | bash
     . ~/.nvm/nvm.sh
     nvm install v10.11.0
+    nvm use --delete-prefix v10.11.0
     npm install
     npm install -g nodemon pm2
     pm2 restart ecosystem.config.js
