70d96f0bf8859d63efda60c9f9b42efac10283ec
Ubuntu <ubuntu@ip-172-31-42-168.us-east-2.compute.internal>
2019-02-22 08:57:32 +0000

    chnage ip

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ssh ubuntu@18.217.98.110 <<EOF
+ssh ubuntu@18.223.111.203 <<EOF
     cd ~/financial-planer
     git pull origin master
     curl -o-   https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh    | bash
