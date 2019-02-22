13224bb1ad0220e140fa0a1bfa00b5c474f57e74
Ubuntu <ubuntu@ip-172-31-42-168.us-east-2.compute.internal>
2019-02-22 08:52:58 +0000

    change shell file

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,11 +2,13 @@
 
 ssh ubuntu@18.217.98.110 <<EOF
     cd ~/financial-planer
-    sudo git pull origin master
+    git pull origin master
     curl -o-   https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh    | bash
     . ~/.nvm/nvm.sh
     nvm install v10.11.0
-    sudo ./script/localdeploy.sh
+    npm install
+    npm install -g nodemon pm2
+    pm2 restart ecosystem.config.js
     exit
 EOF
 
